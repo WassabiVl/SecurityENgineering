a1921cf0abbd8f589d991dc26fa03189262a95f6
wassabivl <wassabi.dietaube@gmail.com>
2018-10-31 14:12:21 +0100

    y

--- hw2/src/card.adb
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO;
-use  Ada.Text_IO;
-
-procedure card is
-   type suit is ('Club', 'Diamond', 'Heart', 'Spades');
-   type deck is ('Ace', '2','3',);
-begin
-   
-end card;
--- hw2/src/main.adb
+++ hw2/src/main.adb
@@ -1,5 +1,18 @@
 procedure Main is
-
+   type Pips is (Two, Three, Four, Five, Six, Seven,
+      Eight, Nine, Ten, Jack, Queen, King, Ace);
+   type Suits is (Diamonds, Spades, Hearts, Clubs);
+   type Card is record
+      Pip : Pips;
+      Suit : Suits;
+      end record;
+   type Index is range 1..53;
+   subtype Deck_Index is Index range 1..52;
+   type Deck_Reference is array(Deck_Index) of Deck_Index;
+   type Deck is record
+      Next_Card : Index;
+      Deck_Offsets : Deck_Reference;
+   end record;
 begin
    --  Insert code here.
    null;
