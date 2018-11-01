19b645242ed91548bab816d92bbd082ac295a24e
waelatrash <wa@lombego.de>
2018-11-01 13:01:42 +0100

    a

--- hw2/src/main.adb
+++ hw2/src/main.adb
@@ -1,4 +1,8 @@
+WITH Ada.Text_IO;
+WITH Ada.Numerics.Discrete_Random;
+use Ada.Text_IO, Ada.Numerics;
 procedure Main is
+
    type Pips is (Two, Three, Four, Five, Six, Seven,
       Eight, Nine, Ten, Jack, Queen, King, Ace);
    type Suits is (Diamonds, Spades, Hearts, Clubs);
@@ -14,6 +18,9 @@ procedure Main is
       Deck_Offsets : Deck_Reference;
    end record;
 begin
-   --  Insert code here.
+   FOR Num IN 1..12 LOOP
+      Put(Discrete_Random.Random(Card));
+      New_Line;
+          end loop;
    null;
 end Main;
