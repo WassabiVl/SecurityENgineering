f83225c3cd01a4cae2b9ac9d1ce76c62a4ed7c39
wassabivl <wassabi.dietaube@gmail.com>
2018-11-07 21:49:32 +0100

    a

--- hw2/src/main.adb
+++ hw2/src/main.adb
@@ -1,9 +1,9 @@
 with Ada.Numerics.Discrete_Random, Playing_Cards;
 with Ada.Text_IO;
 procedure Main is
-   type the_deck is new Deck;
+   type the_deck is new Playing_Cards.Deck;
 begin
-   the_deck := New_Deck;
+   the_deck := Playing_Cards.New_Deck;
    the_deck := Shuffle(the_deck);
    put(Deal(the_deck));
 end Main;
