2ffd29ec2a5f6c1123efc1cc7bd53344890e1fc9
wassabivl <wassabi.dietaube@gmail>
2018-12-04 21:40:29 +0100

    i

--- hw4/src/fibonacci.adb
+++ hw4/src/fibonacci.adb
@@ -1,14 +1,20 @@
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-
-   function Fibonacci (N : Natural) return Natural is
-      A : Natural := 0;
-      B : Natural := 1;
-      Sum : Natural;
-   begin
-      for I in 1..N loop
-         Sum := A + B;
-         B := A;
-         A := Sum;
-      end loop;
-      return A;
-   end Fibonacci;
+-- precondition N >0
+function Fibonacci  (N : Natural ) return Natural is 
+   A : Natural := 0; 
+   B : Natural := 1;
+   Sum : Natural ;
+   X: Natural:= N;
+begin
+   pragma Loop_Invariant (Sum = A);
+   pragma Loop_Variant (Increases => A, Decreases => X);
+   for I in 1.. N loop 
+      Sum := A + B; 
+      B := A;
+      A := Sum;
+      X := N - I;
+   end loop;
+ return A;
+   
+end Fibonacci;
+-- Post condition Fibonacci'Result = A(n-1) + A(n-2)
+-- proof loop terminates when X = 1 or when I = N as no other vairable effect's on N
