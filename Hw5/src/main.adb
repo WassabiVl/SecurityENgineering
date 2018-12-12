cb825fb40345eca9ddbaf8992e9466b76ff2e8d2
wassabivl <wassabi.dietaube@gmail>
2018-12-12 23:13:39 +0100

    working code

--- Hw5/src/main.adb
+++ Hw5/src/main.adb
@@ -1,6 +1,14 @@
-procedure Main is
+with ticket_machine_wael, Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO;
+use ticket_machine_wael, Ada.Text_IO, Ada.Integer_Text_IO;
 
+procedure Main is
+   x : State;
+   y : ticket_machine_wael.Reaction;
 begin
-   --  Insert code here.
-   null;
+   ticket_machine_wael.Initialize(S => x);
+   ticket_machine_wael.Do_Action(S => x,A => Insert_Twenty_Eur,R => y);
+   Ada.Integer_Text_IO.Put(x);
+   ticket_machine_wael.Do_Action(S => x,A => Insert_Five_Eur,R => y);
+   Ada.Integer_Text_IO.Put(x);
 end Main;
