38a6adcd93b9b33336bc88d6340a3bf999292501
waelatrash <wa@lombego.de>
2018-12-20 16:39:24 +0100

    threads

--- Hw5/src/thread.adb
+++ Hw5/src/thread.adb
@@ -1,7 +1,9 @@
+with Ada.Text_IO; use Ada.Text_IO;
 package body Thread is
 procedure Initialize is 
 begin 
-S := Ready;
+      S := Ready;
+      Put_Line('state  changed to Ready');
 end Initialize;
    
 
