19ba9084e6c1087ba686e9a9101aaed12cb03833
wassabivl <wassabi.dietaube@gmail>
2019-01-16 18:02:14 +0100

    tast2

--- hw6/src/sorting.ads
+++ hw6/src/sorting.ads
@@ -5,5 +5,8 @@ is
 
    procedure Selection_Sort(A: in out Natural_Array) with
      Pre => ( A'First > 0 and A'Last < Integer'Last and  A'Length > 1 and A'First /= A'Last ),
-     Post => (A'Old'Length = A'Length and A'First /= A'Last);
+     Post => (A'Old'Length = A'Length and A'First /= A'Last and
+     (for all I in A'First..A'Length =>
+        (for all J in 1+I..A'Length =>
+             (if I < J then A (I) < A (J)))));
 end Sorting;
