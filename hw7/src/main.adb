56e97895d1b11c0f9c903207de111c1add8b0dcc
Ro33ah <Rolzies@gmail.com>
2019-01-29 19:59:47 +0100

    updating Hofstadter

--- hw7/src/main.adb
+++ hw7/src/main.adb
@@ -1,6 +1,16 @@
+with Hofstadter_Q_Sequence;
+with Ada.Text_IO, Ada.Integer_Text_IO; use Ada.Text_IO, Ada.Integer_Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
+
 procedure Main is
+   Input_Number: Integer;
+   Input_TimeOut: Integer;
 
 begin
-   --  Insert code here.
-   null;
+   Put_Line("Number? ");
+   Get(Input_Number);
+   Put_Line("Timeout? ");
+   Get(Input_TimeOut);
+
+   Hofstadter_Q_Sequence.Mute_Workers(Input_Number, Input_TimeOut);
 end Main;
