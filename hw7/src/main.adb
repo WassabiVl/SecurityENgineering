129bb229c1db2ad4d400ddb425da7d84d49ef17c
wassabivl <wassabi.dietaube@gmail>
2019-01-31 18:01:30 +0100

    fix for rosemary

--- hw7/src/main.adb
+++ hw7/src/main.adb
@@ -1,14 +1,31 @@
 with Hofstadter_Q_Sequence;
 with Ada.Text_IO, Ada.Integer_Text_IO; use Ada.Text_IO, Ada.Integer_Text_IO;
-<<<<<<< HEAD
+with Ada.Task_Identification; use Ada.Task_Identification;
 with GNAT.Command_Line; use GNAT.Command_Line;
-
-=======
 with Ada.Command_Line; use Ada.Command_Line;
 with Graph_Algorithms;
 with Graph;
->>>>>>> 33f646882d32c711c69d6d41857a447673763995
 procedure Main is
+   ----------------------hof-------------------------
+   ENVIRONMENT_TASK: constant Task_ID := Current_Task;
+   MAX_RUN_TIME_IN_SECONDS: Duration := 150.0;
+   USER_INPUT_DELAY_IN_SECONDS: constant Duration := 0.1;
+   task User_Input_Task;
+   task body User_Input_Task is
+      Available: Boolean;
+      Char: Character;
+   begin
+      loop
+         Get_Immediate(Char, Available);
+         if Available and (Char = 'q' or else Char = 'Q') then
+            Abort_Task(ENVIRONMENT_TASK);
+         else
+            delay USER_INPUT_DELAY_IN_SECONDS;
+         end if;
+      end loop;
+   end User_Input_Task;
+
+   -------------------graph_algo-------------------------------------
    procedure My_Put(Item : Character) is
    begin
       Put(Item);
@@ -54,7 +71,12 @@ begin -- Main
    Get(Input_Number);
    Put_Line("Timeout? ");
    Get(Input_TimeOut);
-
-   Hofstadter_Q_Sequence.Mute_Workers(Input_Number, Input_TimeOut);
+   MAX_RUN_TIME_IN_SECONDS := Duration(Input_TimeOut);
+   select
+      delay MAX_RUN_TIME_IN_SECONDS;
+   then abort
+      Hofstadter_Q_Sequence.Mute_Workers(Input_Number, Input_TimeOut);
+   end select;
+   Abort_Task(ENVIRONMENT_TASK);
 
 end Main;
